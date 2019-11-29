5c8316f7a3e3cfaa94c266cc419662aac8834f14
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-11-29 13:30:01 +0800

    '配置文件'

---
+++ flowerconfig.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/4 10:04
+# @Author  : lixiaofeng
+# @Site    : 
+# @File    : flowerconfig.py
+# @Software: PyCharm
+
+# Broker settings
+BROKER_URL = 'amqp:redis://localhost:6379/0'
+
+# RabbitMQ management api
+broker_api = 'http://guest:guest@39.105.136.231:15672/api/'
+
+# Enable debug logging
+logging = 'INFO'
+
+# 持久模式
+persistent = True
+
+# 最大
+max_tasks = 100
