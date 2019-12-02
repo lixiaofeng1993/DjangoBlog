978e4d9c857f140c46047bbae55b0e6f43d566d1
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-12-02 11:31:41 +0800

    'csrf'

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
